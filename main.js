ee874f42dee08916496a1212b5d9fcf7ee9958ae
sherena99 <hiroshimasherena@gmail.com>
2022-10-12 02:29:36 +0530

    add pokeball to the field

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const NUM_PLANTS = 50
+const NUM_POKEBALLS= 6
 
 
 function createPlants(){
@@ -10,9 +11,20 @@ function createPlants(){
         document.body.appendChild(div)
     }
 }
+function createPokeBalls(){
+    for(let i=0; i<NUM_POKEBALLS; i++){
+        const div = document.createElement('div')
+        div.classList.add('pokeBall')
+        div.style.left = Math.random() * 100 + '%'
+        div.style.top = Math.random() * 100 + '%'
+        document.body.appendChild(div)
+    }
+
+}
 
 function init(){
     createPlants()
+    createPokeBalls()
 }
 
 init()
