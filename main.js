22311fcbe0f8fdc86ff5daf97c149ab9e71ba7fa
sherena99 <hiroshimasherena@gmail.com>
2022-10-12 08:42:00 +0530

    player moving up and down to the pokeballs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const NUM_PLANTS = 50
-const NUM_POKEBALLS= 6
+const NUM_POKEBALLS = 6
 
 const player = document.querySelector('.player')
 const player_pos = {
@@ -10,6 +10,8 @@ const player_vel = {
     x: 0,
     y: 0
 }
+const balls = []
+
 
 function createPlants(){
     for(let i = 0; i < NUM_PLANTS; i++){
@@ -20,15 +22,57 @@ function createPlants(){
         document.body.appendChild(div)
     }
 }
-function createPokeBalls(){
-    for(let i=0; i<NUM_POKEBALLS; i++){
-        const div = document.createElement('div')
-        div.classList.add('pokeBall')
-        div.style.left = Math.random() * 100 + '%'
-        div.style.top = Math.random() * 100 + '%'
-        document.body.appendChild(div)
-    }
 
+function generateBall(){
+    const div = document.createElement('div')
+    div.classList.add('pokeBall')
+    let x = Math.random() * 100 + '%'
+    let y = Math.random() * 100 + '%'
+    div.style.left = x
+    div.style.top = y
+    balls.push({
+        ball: div,
+        pos: {
+            x,
+            y
+        }
+    })
+    document.body.appendChild(div)
+}
+
+function createPokeBalls(){
+    for(let i = 0; i < NUM_POKEBALLS; i++){
+        generateBall()
+    }
+}
+
+function collision($div1, $div2) {
+    var x1 = $div1.getBoundingClientRect().left;
+    var y1 = $div1.getBoundingClientRect().top;
+    var h1 = $div1.clientHeight;
+    var w1 = $div1.clientWidth;
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+
+    var x2 = $div2.getBoundingClientRect().left;
+    var y2 = $div2.getBoundingClientRect().top;
+    var h2 = $div2.clientHeight;
+    var w2 = $div2.clientWidth;
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+}
+
+function checkCollisions(){
+    balls.forEach(ball => {
+        if(collision(ball.ball, player)){
+            sound.play()
+            ball.ball.remove()
+            generateBall()
+        }
+    })
 }
 
 function run(){
@@ -42,7 +86,6 @@ function run(){
 
     requestAnimationFrame(run)
 }
-
 
 function init(){
     createPlants()
@@ -76,3 +119,83 @@ window.addEventListener('keyup', function(){
     player_vel.y = 0
     player.classList.remove('active')
 })
+
+
+// const NUM_PLANTS = 50
+// const NUM_POKEBALLS= 6
+
+// const player = document.querySelector('.player')
+// const player_pos = {
+//     x: parseInt(window.innerWidth / 2),
+//     y: parseInt(window.innerHeight / 2)
+// }
+// const player_vel = {
+//     x: 0,
+//     y: 0
+// }
+
+// function createPlants(){
+//     for(let i = 0; i < NUM_PLANTS; i++){
+//         const div = document.createElement('div')
+//         div.classList.add('plant')
+//         div.style.left = Math.random() * 100 + '%'
+//         div.style.top = Math.random() * 100 + '%'
+//         document.body.appendChild(div)
+//     }
+// }
+// function createPokeBalls(){
+//     for(let i=0; i<NUM_POKEBALLS; i++){
+//         const div = document.createElement('div')
+//         div.classList.add('pokeBall')
+//         div.style.left = Math.random() * 100 + '%'
+//         div.style.top = Math.random() * 100 + '%'
+//         document.body.appendChild(div)
+//     }
+
+// }
+
+// function run(){
+//     player_pos.x += player_vel.x
+//     player_pos.y += player_vel.y
+
+//     player.style.left = player_pos.x + 'px'
+//     player.style.bottom = player_pos.y + 'px'
+    
+//     checkCollisions()
+
+//     requestAnimationFrame(run)
+// }
+
+
+// function init(){
+//     createPlants()
+//     createPokeBalls()
+//     run()
+// }
+
+// init()
+
+// window.addEventListener('keydown', function(e){
+//     if(e.key == "ArrowUp"){
+//         player_vel.y = 3
+//         player.style.backgroundImage = 'url("assets/player_front.png")'
+//     }
+//     if(e.key == "ArrowDown"){
+//         player_vel.y = -3
+//         player.style.backgroundImage = 'url("assets/player_back.png")'
+//     }
+//     if(e.key == "ArrowLeft"){
+//         player_vel.x = -3
+//         player.style.backgroundImage = 'url("assets/player_left.png")'
+//     }
+//     if(e.key == "ArrowRight"){
+//         player_vel.x = 3
+//         player.style.backgroundImage = 'url("assets/player_right.png")'
+//     }
+//     player.classList.add('active')
+// })
+// window.addEventListener('keyup', function(){
+//     player_vel.x = 0
+//     player_vel.y = 0
+//     player.classList.remove('active')
+// })
